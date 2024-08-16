3aeecdddfa1faccbb29dc1c95b878952156984f7
nickmoreton <nick.moreton@torchbox.com>
2024-08-16 13:44:51 +0100

    Small updates

--- src/renderer/src/ChooseFolder.js
+++ src/renderer/src/ChooseFolder.js
@@ -6,7 +6,7 @@ class ChooseFolder {
   }
 
   init() {
-    this.button.addEventListener('click', () => {
+    this.button.addEventListener('click', (event) => {
       this.input.value = ''
       this.el.dispatchEvent(
         new CustomEvent('folder-selecting', {
@@ -16,29 +16,27 @@ class ChooseFolder {
         })
       )
       window.api.openDialogDirectory().then((result) => {
+        this.button.setAttribute('aria-busy', 'true')
         if (result.canceled) {
           this.input.value = ''
+          event.target.removeAttribute('aria-busy')
           console.log('No folder selected')
           return
         }
 
         this.input.value = result.filePaths[0]
-        this.fileList = window.api
-          .fileList(result.filePaths[0])
-          .then((result) => {
-            return result
-          })
-          .then((result) => {
-            this.result = result
-            this.el.dispatchEvent(
-              new CustomEvent('folder-selected', {
-                detail: this.result,
-                bubbles: true,
-                cancelable: true,
-                composed: true
-              })
-            )
-          })
+        this.fileList = window.api.fileList(result.filePaths[0]).then((result) => {
+          this.result = result
+          this.el.dispatchEvent(
+            new CustomEvent('folder-selected', {
+              detail: this.result,
+              bubbles: true,
+              cancelable: true,
+              composed: true
+            })
+          )
+          this.button.removeAttribute('aria-busy')
+        })
       })
     })
   }
